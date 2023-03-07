3ec514ed97255e18298dbeee21260b8e0faa4b70
Russell Bonner <russell.bonner@microfocus.com>
2023-03-07 20:58:43 +0000

    Update to BBANK10P

--- sources/cobol/core/BBANK10P.cbl
+++ sources/cobol/core/BBANK10P.cbl
@@ -20,7 +20,7 @@
       *****************************************************************
       * Program:     BBANK10P.CBL                                     *
       * Layer:       Business logic                                   *
-      * Function:    Signon to system to identify user  v1            *
+      * Function:    Signon to system to identify user  v2            *
       *****************************************************************
 
        IDENTIFICATION DIVISION.
