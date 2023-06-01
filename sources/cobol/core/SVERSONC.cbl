707e1f36bee2151a42b17c6341f26c1fae54e977
Russell Bonner <russell.bonner@microfocus.com>
2023-06-01 09:09:54 +0000

    Revert to default

--- sources/cobol/core/SVERSONC.cbl
+++ sources/cobol/core/SVERSONC.cbl
@@ -39,7 +39,7 @@
       * Version to show on screens                                    *
       *****************************************************************
        01  WS-VERSION                              PIC X(7)
-           VALUE '23Mar23'.
+           VALUE '31May23'.
 
        LINKAGE SECTION.
        01  DFHCOMMAREA.
