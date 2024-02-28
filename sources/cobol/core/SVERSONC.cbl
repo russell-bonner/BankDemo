1a3aa28a84ca063b3ec95c40daf50febf45c10e0
Russell Bonner <russell.bonner@microfocus.com>
2024-02-28 11:39:19 +0000

    21

--- sources/cobol/core/SVERSONC.cbl
+++ sources/cobol/core/SVERSONC.cbl
@@ -39,7 +39,7 @@
       * Version to show on screens                                    *
       *****************************************************************
        01  WS-VERSION                              PIC X(7)
-           VALUE '22Feb24'.
+           VALUE '21Feb24'.
 
        LINKAGE SECTION.
        01  DFHCOMMAREA.
